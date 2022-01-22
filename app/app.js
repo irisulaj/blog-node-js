84cb0d2724652ab31e04444a245af5e709010842
irisulaj <sulaj.shaqir1@gmail.com>
2022-01-22 23:12:28 +0100

    Added update, delete and insert functions

--- app/app.js
+++ app/app.js
@@ -1,12 +1,17 @@
+const Routes = require("./routes.js")
 const express = require('express')
 const app = express()
 
-const Routes = require('./routes.js')
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
+
+
+
+app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-
+app.use(methodOverride('_method'))
 app.use("/", Routes)
-
 
 
 app.listen(3000, () => {
--- app/routes.js
+++ app/routes.js
@@ -3,32 +3,63 @@ const router = express.Router()
 
 const DB = require("../database/connection.js")
 
+const Prevention = require("sqlstring")
+
+
 router.get("/", (req, res) =>{
-  DB.query(`SELECT * FROM posts`, (err, getPosts)=>{
+  DB.query(`SELECT * FROM posts`, (err, blogPost)=>{
 
     if(err){
 
         console.log(err)
     }else{
 
-        res.render("home.ejs", {getPosts})
+        res.render("home.ejs", {blogPost})
     }
 
 
   })
 })
 
+router.get("/create", (req, res) =>{
+
+  res.render("create.ejs")
+
+})
+
+router.post("/create", (req, res) =>{
+
+  const post = req.body
+
+  DB.query(`INSERT INTO posts (title, image_url, content)
+   VALUES (${Prevention.escape(post.title)}, ${Prevention.escape(post.image_url)}, 
+   ${Prevention.escape(post.content)})`, (err, result)=>{
+      if(err){
+
+          console.log(err)
+      }else{
+
+        res.redirect("/")
+      
+      }
+
+
+  })
+  
+})
+
+
 router.get("/show/:post_id", (req, res) =>{
 
     const post_id = req.params.post_id
 
-    DB.query(`SELECT * FROM posts WHERE post_id = "${post_id}" LIMIT 1`, (err, getPost)=>{
+    DB.query(`SELECT * FROM posts WHERE post_id = "${post_id}" LIMIT 1`, (err, blog)=>{
         if(err){
 
             console.log(err)
         }else{
   
-          res.render("show.ejs", {getPost})
+          res.render("show.ejs", {blog})
         }
 
   
@@ -42,13 +73,14 @@ router.get("/show/:post_id", (req, res) =>{
 
     const posts = req.body
 
-    DB.query(`SELECT * FROM posts WHERE post_id = "${post_id}" LIMIT 1`, (err, getPost)=>{
+    DB.query(`SELECT * FROM posts WHERE post_id = "${post_id}" LIMIT 1`, (err, blog)=>{
         if(err){
 
             console.log(err)
         }else{
   
-          res.render("edit.ejs", {getPost})
+          res.render("edit.ejs", {blog})
+        
         }
 
   
@@ -56,4 +88,47 @@ router.get("/show/:post_id", (req, res) =>{
     
   })
 
+
+  router.put("/edit/:post_id", (req, res) =>{
+
+    const post = req.body
+    const post_id = req.params.post_id
+
+    DB.query(`UPDATE posts SET title=${Prevention.escape(post.title)}, image_url=${Prevention.escape(post.image_url)},
+    content=${Prevention.escape(post.content)}
+    WHERE post_id= "${post_id}"`, (err, result)=>{
+        if(err){
+console.log(err)
+   
+        }else{
+
+              console.log("Updated")
+              console.log(post)
+             
+         res.redirect("/")
+        }
+
+  
+    })
+    
+  })
+
+  router.get("/delete/:post_id", (req, res) =>{
+
+    const post_id = req.params.post_id
+
+    DB.query(`DELETE FROM posts WHERE post_id = "${post_id}"`, (err, result)=>{
+        if(err){
+console.log(err)   
+        }else{
+              console.log("Blog post delete!")
+         res.redirect("/")
+        }
+
+  
+    })
+    
+  })
+
+  
 module.exports = router
